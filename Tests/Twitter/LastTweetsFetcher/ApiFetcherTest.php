e6eade34f97afa086e6fae864604dff85c6376ee
Evgenij Guseletov <seo-in@ya.ru>
2012-04-05 18:34:01 +0300

    Add new tests

--- Tests/Twitter/LastTweetsFetcher/ApiFetcherTest.php
+++ Tests/Twitter/LastTweetsFetcher/ApiFetcherTest.php
@@ -9,6 +9,29 @@ class ApiFetcherTest extends \PHPUnit_Framework_TestCase
 {
     const CLASSNAME = 'Knp\Bundle\LastTweetsBundle\Twitter\LastTweetsFetcher\ApiFetcher';
     
+    public function testRealFetchWithLimit()
+    {
+        $fixture = json_encode(array(
+            'one' => array('id' => 1, 'id_str' => 1, 'text' => 'asdasdasd', 'created_at' => 'Thu Apr 04 11:58:04 +0000 2012'), 
+            'two' => array('id' => 2, 'id_str' => 2, 'text' => 'asdasdasd2', 'created_at' => 'Thu Apr 05 12:45:43 +0000 2012'),
+            'three' => array('id' => 3, 'id_str' => 3, 'text' => 'asdasdasd3', 'created_at' => 'Thu Apr 06 13:22:28 +0000 2012'),
+            'four' => array('id' => 4, 'id_str' => 4, 'text' => 'asdasdasd4', 'created_at' => 'Thu Apr 07 14:36:01 +0000 2012')
+        ));
+        
+        $mockedBrowser = $this->getMockedBrowser();
+        $mockedResponse = $this->getMockedResponse($fixture);
+        
+        $mockedBrowser->expects($this->exactly(2))
+            ->method('get')
+            ->will($this->returnValue($mockedResponse));
+        
+        $fetcher = new ApiFetcher($mockedBrowser);
+        
+        $tweets = $fetcher->fetch(array('knplabs', 'knplabsru'), 4);
+        
+        $this->assertCount(4, $tweets);
+    }
+    
     public function testMultiAccountFetch()
     {
         $mockedTweet = $this->getMockedTweet();
@@ -19,7 +42,7 @@ class ApiFetcherTest extends \PHPUnit_Framework_TestCase
             'three' => array('id_str' => 3, 'text' => 'asdasdasd3')
         ));
         
-        $fetcher = $this->getMockedFetcher($fixture, 3);
+        $fetcher = $this->getMockedFetcher($fixture);
         
         $fetcher->expects($this->exactly(3))
             ->method('createTweet')
@@ -27,7 +50,7 @@ class ApiFetcherTest extends \PHPUnit_Framework_TestCase
         
         $tweets = $fetcher->fetch(array('knplabs', 'knplabsru'), 3);
         
-        $this->assertEquals(3, count($tweets));   
+        $this->assertCount(3, $tweets);   
     }
     
     public function testFetchTweetCreation()
@@ -85,7 +108,7 @@ class ApiFetcherTest extends \PHPUnit_Framework_TestCase
 
         $tweets = $fetcher->fetch('knplabs', 3);
 
-        $this->assertEquals(3, count($tweets));
+        $this->assertCount(3, $tweets);
     }
     
     /**
@@ -101,7 +124,6 @@ class ApiFetcherTest extends \PHPUnit_Framework_TestCase
 
         $fetcher->expects($this->once())
             ->method('getContents')
-            ->with($this->stringContains('http://api.twitter.com/1/statuses/user_timeline.json'))
             ->will($this->returnValue(null));
 
         $tweets = $fetcher->fetch('knplabs');
@@ -120,7 +142,6 @@ class ApiFetcherTest extends \PHPUnit_Framework_TestCase
 
         $fetcher->expects($this->once())
             ->method('getContents')
-            ->with($this->stringContains('http://api.twitter.com/1/statuses/user_timeline.json'))
             ->will($this->returnValue('a{'));
 
         $tweets = $fetcher->fetch('knplabs');
@@ -131,7 +152,7 @@ class ApiFetcherTest extends \PHPUnit_Framework_TestCase
         return $this->getMock('Knp\Bundle\LastTweetsBundle\Twitter\Tweet', array(), array(), '', false);
     }
 
-    protected function getMockedFetcher($fixture, $count = 10)
+    protected function getMockedFetcher($fixture)
     {
         $fetcher = $this->getMock(
             self::CLASSNAME,
@@ -141,7 +162,6 @@ class ApiFetcherTest extends \PHPUnit_Framework_TestCase
 
         $fetcher->expects($this->atLeastOnce())
             ->method('getContents')
-            ->with($this->stringContains('http://api.twitter.com/1/statuses/user_timeline.json'))
             ->will($this->returnValue($fixture));
 
         return $fetcher;
@@ -153,4 +173,15 @@ class ApiFetcherTest extends \PHPUnit_Framework_TestCase
         
         return $browser;
     }
+    
+    protected function getMockedResponse($fixture)
+    {
+        $response = $this->getMock('Buzz\Browser\Message\Response', array('getContent'));
+        
+        $response->expects($this->atLeastOnce())
+            ->method('getContent')
+            ->will($this->returnValue($fixture));
+        
+        return $response;
+    }   
 }
