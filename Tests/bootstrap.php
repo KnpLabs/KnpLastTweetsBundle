b8b6b1eb9ef717f0a967cd2277170d43dee689c4
Evgeniy Guseletov <seo-in@ya.ru>
2012-04-13 15:55:04 +0300

    Update bootstrap and composer.json

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,11 +1,22 @@
 <?php
 
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-$loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
-$loader->registerNamespace('Knp\\Bundle\\LastTweetsBundle', __DIR__.'/../../../..');
-$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
-$loader->registerPrefixes(array(
-    'Twig_'            => $_SERVER['TWIG'],
-));
-$loader->register();
+$vendorDir = __DIR__ . '/../vendor';
+ 
+if (!@include($vendorDir . '/.composer/autoload.php')) {
+    die("You must set up the project dependencies, run the following commands:
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+");
+}
+ 
+spl_autoload_register(function($class) {
+    if (0 === (strpos($class, 'Knp\\Bundle\\LastTweetsBundle\\'))) {
+        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 3)).'.php';
+ 
+        if (!stream_resolve_include_path($path)) {
+            return false;
+        }
+        require_once $path;
+        return true;
+    }
+});
